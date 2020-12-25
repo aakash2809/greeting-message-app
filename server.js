08d1b6c9d6ff47ec702665ca04fbb6a623db1d56
aakash2809 <aakashrajak289@gmail.com>
2020-12-26 00:55:55 +0530

    add: Database configuration and folder structure

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 const express = require(`express`);
+const bodyParser = require('body-parser');
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
 
 /**
  * @description create express app
  */
 const app = express();
+
+/**
+ * @description parse requests 
+ */
+app.use(bodyParser.urlencoded({ extended: true }))
+
+/**
+ * @description parse requests of content-type - application/json
+ */
+app.use(bodyParser.json())
+
+mongoose.Promise = global.Promise;
+
+/**
+ * @description Connecting to the database
+ */
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
 
 /**
  * @description define a simple route
