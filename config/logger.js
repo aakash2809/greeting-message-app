ca1366d81116777fd693a3431a16fb703cb23166
aakash2809 <aakashrajak289@gmail.com>
2021-01-02 01:37:02 +0530

    add: log file using winston File Transport

--- config/logger.js
+++ config/logger.js
@@ -1,15 +1,25 @@
-const {createLogger,format,transports} = require('winston');
+const { createLogger, format, transports } = require('winston');
 
 const logger = createLogger({
-    level: 'info',
-    format:format.combine(
+  level: 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+  ),
+  transports: [
+    new transports.Console({
+      level: 'info',
+      format: format.combine(
         format.colorize(),
-        format.timestamp({
-          format: 'YYYY-MM-DD HH:mm:ss'
-        }),
-        format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-      ),
-    transports: [new transports.Console()]
+        format.printf(
+          info => `${info.timestamp} ${info.level}: ${info.message}`
+        )
+      )
+    }),
+    new transports.File({ filename: 'greeting.log' })
+  ]
 });
 
 module.exports = logger;
