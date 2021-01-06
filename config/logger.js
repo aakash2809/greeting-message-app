ca05dea3452b626e698a13eaa8c8a800b8afcdfe
aakash2809 <aakashrajak289@gmail.com>
2021-01-06 12:28:48 +0530

    modify: logger.js, remove console transport

--- config/logger.js
+++ config/logger.js
@@ -2,42 +2,7 @@ const winston = require('winston');
 
 const logger = winston.createLogger({
   transports: [
-    new winston.transports.Console({
-      level: 'info',
-      format: winston.format.combine(
-        winston.format.colorize(), winston.format.timestamp({
-          format: 'YYYY-MM-DD HH:mm:ss'
-        }),
-        winston.format.printf(
-          info => `${info.timestamp} ${info.level}: ${info.message}`
-        )
-      )
-    }),
-
-    /* new transports.File({ 
-      level: 'info',
-      filename: './logs/info.log',
-      format: format.combine(
-       format.timestamp({
-         format: 'YYYY-MM-DD HH:mm:ss'
-       }),
-       format.printf(
-         info => `${info.timestamp} ${info.level}: ${info.message}`
-       )
-     ) }),
-    new transports.File({ 
-      level: 'error',
-      filename: './logs/error.log',
-      format: format.combine(
-       format.timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss'
-      }),
-       format.printf(
-         error => `${error.timestamp} ${error.level}: ${error.message}`
-       )
-     ) }), */
-
-    new winston.transports.File({
+     new winston.transports.File({
       level: 'info',
       filename: './logs/info.log',
       format: winston.format.combine(
