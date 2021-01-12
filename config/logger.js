4ff1bda021eb05690d7d02cfb5e1a160d17b982d
aakash2809 <aakashrajak289@gmail.com>
2021-01-12 09:43:53 +0530

    modify: swagger.json(remove security part) & tried some rendom operation did not change fucntionality

--- config/logger.js
+++ config/logger.js
@@ -11,19 +11,6 @@ const winston   = require('winston');
 
  const logger    = winston.createLogger({
   transports: [
-     new winston.transports.File({
-      level: 'info',
-      filename: './logs/info.log',
-      format: winston.format.combine(
-        winston.format.timestamp({
-          format: 'YYYY-MM-DD HH:mm:ss'
-        }),
-        winston.format.printf(
-          info => `${info.timestamp} ${info.level}: ${info.message}`
-        )
-      )
-    }),
-
     new winston.transports.File({
       level: 'error',
       filename: './logs/error.log',
@@ -36,7 +23,21 @@ const winston   = require('winston');
           error => `${error.timestamp} ${error.level}: ${error.message}`
         ) 
       )
-    }) ]
+    }) ,
+     new winston.transports.File({
+      level: 'info',
+      filename: './logs/info.log',
+      format: winston.format.combine(
+        winston.format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        winston.format.printf(
+          info => `${info.timestamp} ${info.level}: ${info.message}`
+        )
+      )
+    })
+
+   ]
 }); 
 
 module.exports  = logger;
